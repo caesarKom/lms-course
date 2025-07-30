fb7ca8abc5f88a3134f24a25424f29d9d6e12724
caesarKom <cezarykomorowski@gmail.com>
2025-07-30 13:02:41 +0200

    arcjet

---
+++ app/(public-route)/page.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+
+export default function PublicHomePage() {
+  return (
+    <>
+      <section className="relative py-20">
+        <div>
+          <Badge>The Future of Online Education</Badge>
+        </div>
+      </section>
+    </>
+  );
+}
