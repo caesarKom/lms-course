abece630971d0c1b4b8a74aad5d7263661f62c46
caesarKom <cezarykomorowski@gmail.com>
2025-08-02 18:54:08 +0200

    upload-file

--- app/(public-route)/_components/user-menu.tsx
+++ app/(public-route)/_components/user-menu.tsx
@@ -46,7 +46,7 @@ export default function UserMenu({ name, email, image }: UserProps) {
           />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="max-w-64">
+      <DropdownMenuContent align="end" className="min-w-48">
         <DropdownMenuLabel className="flex min-w-0 flex-col">
           <span className="text-foreground truncate text-sm font-medium">
             {name}
